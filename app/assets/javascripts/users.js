e9e53eaa06da41b3c344b2a1fadcf788edf252fc
nauticalnova <nauticalnova@hotmail.com>
2017-02-22 14:53:49 -0700

    added navbar links

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,16 +2,13 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
-console.log('js loaded')
-$(document).ready(function(){
+$(document).on('turbolinks:load', function() {
 	$('#dropdownMenuButton').on('click', function (event) {
-		console.log("ran")
 	    $('.test').toggleClass('show');
 	    $('.test1').removeClass('show');
 	});
 
 	$('body').on('click', function (e) {
-		console.log("ran on body")
 	    if (!$('#dropdownMenuButton').is(e.target) 
 	        && $('#dropdownMenuButton').has(e.target).length === 0 
 	        && $('.show').has(e.target).length === 0
@@ -20,13 +17,11 @@ $(document).ready(function(){
 	    }
 	});
 	$('#dropdownMenuButton1').on('click', function (event) {
-		console.log("ran")
 	    $('.test1').toggleClass('show');
 	    $('.test').removeClass('show');
 	});
 
 	$('body').on('click', function (e) {
-		console.log("ran on body")
 	    if (!$('#dropdownMenuButton1').is(e.target) 
 	        && $('#dropdownMenuButton1').has(e.target).length === 0 
 	        && $('.show').has(e.target).length === 0
