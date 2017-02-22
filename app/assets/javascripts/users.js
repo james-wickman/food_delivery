0be8c4aecb8effbe292ff7961568ba13483a86ad
nauticalnova <nauticalnova@hotmail.com>
2017-02-22 12:36:09 -0700

    made dropdowns work

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -7,5 +7,31 @@ $(document).ready(function(){
 	$('#dropdownMenuButton').on('click', function (event) {
 		console.log("ran")
 	    $('.test').toggleClass('show');
+	    $('.test1').removeClass('show');
+	});
+
+	$('body').on('click', function (e) {
+		console.log("ran on body")
+	    if (!$('#dropdownMenuButton').is(e.target) 
+	        && $('#dropdownMenuButton').has(e.target).length === 0 
+	        && $('.show').has(e.target).length === 0
+	    ) {
+	        $('.test').removeClass('show');
+	    }
+	});
+	$('#dropdownMenuButton1').on('click', function (event) {
+		console.log("ran")
+	    $('.test1').toggleClass('show');
+	    $('.test').removeClass('show');
+	});
+
+	$('body').on('click', function (e) {
+		console.log("ran on body")
+	    if (!$('#dropdownMenuButton1').is(e.target) 
+	        && $('#dropdownMenuButton1').has(e.target).length === 0 
+	        && $('.show').has(e.target).length === 0
+	    ) {
+	        $('.test1').removeClass('show');
+	    }
 	});
 });
