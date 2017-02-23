72e212c7d98f8c1212208e0125fea0269aacc15d
nauticalnova <nauticalnova@hotmail.com>
2017-02-22 17:47:06 -0700

    added avatar and driver_avatar

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -40,17 +40,16 @@ before_action :configure_account_update_params, only: [:update]
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :avatar, :address, :city, :state, :zip, :phone_number, :organic, :generic])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :avatar, :address, :city, :state, :zip, :phone_number, :organic, :generic])
   end
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    byebug
     users_show_path
   end
 
