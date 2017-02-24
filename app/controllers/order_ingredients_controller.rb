7841a5eba61d45a668c29068ea9248ea661afa6e
Jonathan Fox <jonathan.fox1991@gmail.com>
2017-02-24 13:42:52 -0700

    totals on cart display

--- app/controllers/order_ingredients_controller.rb
+++ app/controllers/order_ingredients_controller.rb
@@ -1,7 +1,6 @@
 class OrderIngredientsController < ApplicationController
   def index
     @order = current_order
-
   end
 
   def new
@@ -17,6 +16,13 @@ class OrderIngredientsController < ApplicationController
   end
 
   def update
+    @order = current_order
+    @order_ingredient = OrderIngredient.find(order_ingredient_params[:id])
+    if @order_ingredient.update_attributes(order_ingredient_params)
+      respond_to do |format|
+        format.js
+      end
+    end
   end
 
   def destroy
@@ -27,5 +33,11 @@ class OrderIngredientsController < ApplicationController
         format.js
       end
     end
+  end
+
+  private
+
+  def order_ingredient_params
+    params.permit(:id, :quantity)
   end
 end
